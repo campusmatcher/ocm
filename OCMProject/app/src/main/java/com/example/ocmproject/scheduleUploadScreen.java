09b98dceb905a9a64db854528683b47ab6c6cd2c
Alpsencer <alpsencer.ozdemir@ug.bilkent.edu.tr>
2022-04-10 17:05:33 +0300

    test of comments

--- OCMProject/app/src/main/java/com/example/ocmproject/scheduleUploadScreen.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/scheduleUploadScreen.java
@@ -14,6 +14,7 @@ import android.widget.ImageView;
 public class scheduleUploadScreen extends AppCompatActivity {
 
     // Yavuz was here
+    // Also alp was
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
