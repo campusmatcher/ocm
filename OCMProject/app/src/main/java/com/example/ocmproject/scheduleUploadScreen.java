da3ead0d1b1bb68858b37865429351ef34fa866a
Alpsencer <alpsencer.ozdemir@ug.bilkent.edu.tr>
2022-04-10 16:09:48 +0300

    schedule screen push

--- OCMProject/app/src/main/java/com/example/ocmproject/scheduleUploadScreen.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/scheduleUploadScreen.java
@@ -2,7 +2,9 @@ package com.example.ocmproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class scheduleUploadScreen extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class scheduleUploadScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_upload_screen);
+        Button scheduleUpload = findViewById(R.id.scheduleUploadButton);
+        Intent intent = new Intent();
     }
 }
