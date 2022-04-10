37ac8c7070cb316d398a204bffd46c1b7f04e326
Alpsencer <alpsencer.ozdemir@ug.bilkent.edu.tr>
2022-04-10 16:27:52 +0300

    schedule upload page's design completed

--- OCMProject/app/src/main/java/com/example/ocmproject/scheduleUploadScreen.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/scheduleUploadScreen.java
@@ -1,18 +1,42 @@
 package com.example.ocmproject;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class scheduleUploadScreen extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_schedule_upload_screen);
-        Button scheduleUpload = findViewById(R.id.scheduleUploadButton);
-        Intent intent = new Intent();
+        Button scheduleUploadButton = findViewById(R.id.scheduleUploadButton);
+        scheduleUploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 3);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && data != null){
+            Uri imageChoosen = data.getData();
+            ImageView scheduleView = findViewById(R.id.scheduleView);
+            scheduleView.setImageURI(imageChoosen);
+
+        }
+
     }
 }
