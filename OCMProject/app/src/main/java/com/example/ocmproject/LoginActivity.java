46980128437f47f503949f3b7cbb59e2ea0fcf0a
Yavuz Alp Sencer ÖZTÜRK <alpsencerozturk@gmail.com>
2022-04-11 02:34:39 +0300

    Remove action bar from LoginActivity screen

--- OCMProject/app/src/main/java/com/example/ocmproject/LoginActivity.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/LoginActivity.java
@@ -25,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getSupportActionBar().hide();
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
