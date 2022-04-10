bfb50ffde05679fcad96be1b402c8694d2823816
Yavuz Alp Sencer ÖZTÜRK <alpsencerozturk@gmail.com>
2022-04-11 01:35:30 +0300

    Update Logout now working

--- OCMProject/app/src/main/java/com/example/ocmproject/MainActivity.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.ocmproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        logout = findViewById(R.id.logout);
+
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "Logged Out!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, StartActivity.class));
+            }
+
+        });
     }
 }
