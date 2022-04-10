99ae6d14f928d73bc151e4be9fb16cb3a7297981
Yavuz Alp Sencer ÖZTÜRK <alpsencerozturk@gmail.com>
2022-04-11 02:26:30 +0300

    Remove action bar from StartActivity screen

--- OCMProject/app/src/main/java/com/example/ocmproject/StartActivity.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/StartActivity.java
@@ -16,6 +16,7 @@ public class StartActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        getSupportActionBar().hide();
 
         register = findViewById(R.id.register);
         login = findViewById(R.id.login);
