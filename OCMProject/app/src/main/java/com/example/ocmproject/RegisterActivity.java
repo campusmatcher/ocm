af37c2d2a73a29800f5f62170306f5e28621a359
Yavuz Alp Sencer ÖZTÜRK <alpsencerozturk@gmail.com>
2022-04-11 01:15:38 +0300

    Add MainActivity.java

--- OCMProject/app/src/main/java/com/example/ocmproject/RegisterActivity.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.example.ocmproject;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
