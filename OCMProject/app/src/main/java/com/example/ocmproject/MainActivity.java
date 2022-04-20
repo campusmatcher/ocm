bd9c88ac27e42b6d6238d5f343a06ac973d02706
bilal <abdurrahmankar697@gmail.com>
2022-04-20 14:15:57 +0300

    write bilal was here

--- OCMProject/app/src/main/java/com/example/ocmproject/MainActivity.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/MainActivity.java
@@ -18,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     // Yavuz was here
     // Sencer was here
     // bilal was here
+    //again
     private Button logout;
     private EditText edit;
     private Button add;
