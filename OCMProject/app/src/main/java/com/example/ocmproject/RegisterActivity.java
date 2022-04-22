bd40ae421c54831fd04776bde35cc99c2704d39f
Yavuz Alp Sencer ÖZTÜRK <alpsencerozturk@gmail.com>
2022-04-22 19:23:27 +0300

    Fixed typo RegisterActivity.java

--- OCMProject/app/src/main/java/com/example/ocmproject/RegisterActivity.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/RegisterActivity.java
@@ -93,7 +93,7 @@ public class RegisterActivity extends AppCompatActivity {
                             finish();
                         }
                     }
-                })
+                });
             }
         }).addOnFailureListener(new OnFailureListener() {
             @Override
