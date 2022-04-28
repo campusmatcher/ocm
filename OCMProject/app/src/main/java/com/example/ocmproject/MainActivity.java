51a42c194fdd332e2a60d9841bf024203cafe645
Yavuz Alp Sencer ÖZTÜRK <28690608+alpsencer@users.noreply.github.com>
2022-04-28 21:23:58 +0300

    Omega 6

--- OCMProject/app/src/main/java/com/example/ocmproject/MainActivity.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/MainActivity.java
@@ -26,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
     // Sencer was here
     // bilal was here
     // Arda was here
-    // l1234
+    // l12345
     //again
 
     private Button logout;
