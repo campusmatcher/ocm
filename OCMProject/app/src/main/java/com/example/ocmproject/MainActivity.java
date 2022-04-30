dbed6b290ba53c31b45cfd5a5127399f91754fa0
Yavuz Alp Sencer ÖZTÜRK <28690608+alpsencer@users.noreply.github.com>
2022-05-01 02:07:36 +0300

    Added Bottom menu (but now not fully functional)

--- OCMProject/app/src/main/java/com/example/ocmproject/MainActivity.java
+++ OCMProject/app/src/main/java/com/example/ocmproject/MainActivity.java
@@ -2,15 +2,22 @@ package com.example.ocmproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.ocmproject.Fragments.HomeFragment;
+import com.example.ocmproject.Fragments.NotificationFragment;
+import com.example.ocmproject.Fragments.ProfileFragment;
+import com.example.ocmproject.Fragments.SearchFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     // Arda was here
     //again
 
+    private BottomNavigationView bottomNavigationView;
+    private Fragment selectorFragment;
+
     private Button logout;
     private EditText edit;
     private Button add;
@@ -44,100 +54,132 @@ public class MainActivity extends AppCompatActivity {
         edit = findViewById(R.id.edit);
         add = findViewById(R.id.add);
 
-        // Logout button's function
-        logout.setOnClickListener(new View.OnClickListener() {
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                FirebaseAuth.getInstance().signOut();
-                Toast.makeText(MainActivity.this, "Logged Out!", Toast.LENGTH_SHORT).show();
-                startActivity(new Intent(MainActivity.this, StartActivity.class));
-                finish();
-            }
-        });
-
-//         Adding one value
-//         FirebaseDatabase.getInstance().getReference().child("ocmdatabase").child("Android").setValue("abcd");
-
-//        HashMap<String , Object> map = new HashMap<>();
-//        map.put("Name", "Alp");
-//        map.put("Email", "sencer@ocm.com");
-//
-//        FirebaseDatabase.getInstance().getReference().child("Isimler").child("CokluDeger").updateChildren(map);
-
-        // Add button's functions
-        add.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                //Adding eventListener to reference
-//                //Creating and initializing User class object
-//                Abc abc2 = new Abc("user1234", "abc", "xyz", "First", "January", "2001");
-//
-//                //Getting Instance of Firebase realtime database
-//                FirebaseDatabase databaseInstance = FirebaseDatabase.getInstance();
-//
-//                //Getting Reference to a User node, (it will be created if not already there)
-//                DatabaseReference userNode = database.getReference("Abc2");
-//
-//                //Writing the User class object to that reference
-//                userNode.setValue(abc2);
-
-
-//                ArrayList<String> liste = new ArrayList<String>();
-//                liste.add("kelime 1");
-//                liste.add("kelime 2");
-//                liste.add("kelime 3");
-//                liste.add("kelime 4");
-
-                for(int i = 0; i < 40; i++){
-                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").setValue("0");
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.nav_home:
+                        selectorFragment = new HomeFragment();
+                        break;
+                    case R.id.nav_search:
+                        selectorFragment = new SearchFragment();
+                        break;
+                    case R.id.nav_add:
+                        selectorFragment = null;
+                        break;
+                    case R.id.nav_heart:
+                        selectorFragment = new NotificationFragment();
+                        break;
+                    case R.id.nav_profile:
+                        selectorFragment = new ProfileFragment();
+                        break;
                 }
-
-
-
-                myRef.child("Abc").addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        //Getting the string value of that node
-                        //Abc abc2 =  dataSnapshot.getValue(Abc.class);
-                        //Toast.makeText(MainActivity.this, "Data Received: username: " + abc2.getFirstName(), Toast.LENGTH_SHORT).show();
-                        //Log.e("OK", "BASARILI" + abc2.getLastName() );
-
-
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-                        Log.e("Error", "onCancelled: Something went wrong! Error:" + databaseError.getMessage() );
-
-                    }
-                });
-
-
-
-
-//                String txt_name = edit.getText().toString();
-////                ArrayList<String> liste = new ArrayList<String>();
-////                liste.add("Deneme 1");
-//                if(txt_name.isEmpty()) {
-//                    Toast.makeText(MainActivity.this, "No Name Entered!", Toast.LENGTH_SHORT).show();
-//                } else {
-//                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Math102-01").child("day1").child("les1").setValue("0");
-//                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Eng102-01").child("day1").child("les1").setValue("0");
-//                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Cs102-01").child("day1").child("les1").setValue("0");
-//                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Turk102-01").child("day1").child("les1").setValue("0");
-//                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Math132-01").child("day1").child("les1").setValue("0");
-//
-////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day2").child("les2").setValue("0");
-////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day3").child("les3").setValue("0");
-////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day4").child("les4").setValue("1");
-////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day5").child("les5").setValue("1");
-////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day6").child("les6").setValue("0");
-////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day7").child("les7").setValue("1");
-//                    Toast.makeText(MainActivity.this, "Addition completed!", Toast.LENGTH_SHORT).show();
-//                    //startActivity(new Intent(MainActivity.this, ScheduleActivity.class));
+                if (selectorFragment != null){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectorFragment).commit();
+                }
+                return true;
             }
-
         });
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectorFragment).commit();
+
+        // Logout button's function
+//        logout.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                FirebaseAuth.getInstance().signOut();
+//                Toast.makeText(MainActivity.this, "Logged Out!", Toast.LENGTH_SHORT).show();
+//                startActivity(new Intent(MainActivity.this, StartActivity.class));
+//                finish();
+//            }
+//        });
+//
+////         Adding one value
+////         FirebaseDatabase.getInstance().getReference().child("ocmdatabase").child("Android").setValue("abcd");
+//
+////        HashMap<String , Object> map = new HashMap<>();
+////        map.put("Name", "Alp");
+////        map.put("Email", "sencer@ocm.com");
+////
+////        FirebaseDatabase.getInstance().getReference().child("Isimler").child("CokluDeger").updateChildren(map);
+//
+//        // Add button's functions
+//        add.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+////                //Adding eventListener to reference
+////                //Creating and initializing User class object
+////                Abc abc2 = new Abc("user1234", "abc", "xyz", "First", "January", "2001");
+////
+////                //Getting Instance of Firebase realtime database
+////                FirebaseDatabase databaseInstance = FirebaseDatabase.getInstance();
+////
+////                //Getting Reference to a User node, (it will be created if not already there)
+////                DatabaseReference userNode = database.getReference("Abc2");
+////
+////                //Writing the User class object to that reference
+////                userNode.setValue(abc2);
+//
+//
+////                ArrayList<String> liste = new ArrayList<String>();
+////                liste.add("kelime 1");
+////                liste.add("kelime 2");
+////                liste.add("kelime 3");
+////                liste.add("kelime 4");
+//
+//                for(int i = 0; i < 40; i++){
+//                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").setValue("0");
+//                }
+//
+//
+//
+//                myRef.child("Abc").addValueEventListener(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        //Getting the string value of that node
+//                        //Abc abc2 =  dataSnapshot.getValue(Abc.class);
+//                        //Toast.makeText(MainActivity.this, "Data Received: username: " + abc2.getFirstName(), Toast.LENGTH_SHORT).show();
+//                        //Log.e("OK", "BASARILI" + abc2.getLastName() );
+//
+//
+//
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//                        Log.e("Error", "onCancelled: Something went wrong! Error:" + databaseError.getMessage() );
+//
+//                    }
+//                });
+//
+//
+//
+//
+////                String txt_name = edit.getText().toString();
+//////                ArrayList<String> liste = new ArrayList<String>();
+//////                liste.add("Deneme 1");
+////                if(txt_name.isEmpty()) {
+////                    Toast.makeText(MainActivity.this, "No Name Entered!", Toast.LENGTH_SHORT).show();
+////                } else {
+////                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Math102-01").child("day1").child("les1").setValue("0");
+////                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Eng102-01").child("day1").child("les1").setValue("0");
+////                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Cs102-01").child("day1").child("les1").setValue("0");
+////                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Turk102-01").child("day1").child("les1").setValue("0");
+////                    FirebaseDatabase.getInstance().getReference().child("Courses").child("Math132-01").child("day1").child("les1").setValue("0");
+////
+//////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day2").child("les2").setValue("0");
+//////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day3").child("les3").setValue("0");
+//////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day4").child("les4").setValue("1");
+//////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day5").child("les5").setValue("1");
+//////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day6").child("les6").setValue("0");
+//////                    FirebaseDatabase.getInstance().getReference().child("Users").child("hZ86xa3nw1PO8dUmBIW0UTk28R53").child("Schedule").child("day7").child("les7").setValue("1");
+////                    Toast.makeText(MainActivity.this, "Addition completed!", Toast.LENGTH_SHORT).show();
+////                    //startActivity(new Intent(MainActivity.this, ScheduleActivity.class));
+//            }
+//
+//        });
     }
 }
