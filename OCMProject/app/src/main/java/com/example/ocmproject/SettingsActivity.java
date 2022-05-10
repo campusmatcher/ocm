6eae0f74727835f4354eb89970cb5bfc4d36681f
Arda Kırcı <krcarda@gmail.com>
2022-05-10 20:21:37 +0300

    deleted unnecessary activities

---
+++ OCMProject/app/src/main/java/com/example/ocmproject/SettingsActivity.java
@@ -0,0 +1,22 @@
+package com.example.ocmproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+
+    }
+}
